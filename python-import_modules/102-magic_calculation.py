08eacc6d9969dc166db647272b231528d16b84b4
m.ntivuguruzwa <m.ntivuguru@alustudent.com>
2023-02-28 12:48:13 -0800

    Task 8. ByteCode -> Python #3

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
@@ -10,5 +8,3 @@ def magic_calculation(a, b):
         return (c)
     else:
         return (sub(a, b))
-
-
