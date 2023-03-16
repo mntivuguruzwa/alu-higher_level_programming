5f40743061519bdb5bd36779f83aaf0a58cba375
m.ntivuguruzwa <m.ntivuguru@alustudent.com>
2023-03-16 09:32:25 -0700

    Task 1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,48 +1,41 @@
 #!/usr/bin/python3
-"""
-A rectangle with width and height.
-"""
+"""This module creates a class named Rectangle"""
 
 
 class Rectangle:
+    """A class named Rectangle
+    Attributes:
+    attr1(width): width of rectangle
+    attr2(height): height of rectangle
     """
-    Rectangle functions and data
-    """
-
     def __init__(self, width=0, height=0):
-        """ Instantiation
-        """
         self.width = width
         self.height = height
 
     @property
+    def height(self):
+        """Gets the height of the class instance"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of the class instance"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
     def width(self):
-        """ Getter for width
-        """
+        """Gets the width of the class instance"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ Setter for width
-        """
+        """Sets the width of the class instance"""
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
-    @property
-    def height(self):
-        """ Getter for height
-        """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ Setter for height
-        """
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
