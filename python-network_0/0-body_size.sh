3abfd203619e8d7de892632f4b5378971dc0397b
m.ntivuguruzwa <m.ntivuguru@alustudent.com>
2023-04-05 10:34:58 -0700

    Task 0. cURL body size

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -Ls "$1"
+curl -s $1 --include | grep -i Content-Length | awk '{print $2}'
