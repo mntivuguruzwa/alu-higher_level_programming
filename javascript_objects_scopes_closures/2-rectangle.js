c8bf205a3c1f1fb5653d5fb031abc9b213d8487f
m.ntivuguruzwa <m.ntivuguru@alustudent.com>
2023-06-14 15:30:11 -0700

    Task 2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Rectangle class that defines a rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
