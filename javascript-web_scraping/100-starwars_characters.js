aba6c12362fea3fe771eaa869143b92daab15446
m.ntivuguruzwa <m.ntivuguru@alustudent.com>
2023-06-14 16:13:17 -0700

    Task Who was playing in this movie?

---
+++ javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// Script that gets the contents of a webpage and stores it in a file.
+
+const request = require('request');
+const endPoint = 'http://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request.get(endPoint, function (err, response, body) {
+  if (err) {
+    throw err;
+  } else if (response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach(character => {
+      request.get(character, function (err, response, body) {
+        if (err) {
+          throw err;
+        } else if (response.statusCode === 200) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});
